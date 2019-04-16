7f7901af07749a6929fba127a790fda4688357e5
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2019-04-16 13:47:14 -0400

    Made the keys appear and every not has a different sound

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -40,10 +40,11 @@ public class Piano extends World
         
         
             //condition 1         //condition 2  
-        if((frames % 60 == 0) && (position < 14) ) {
+        if((frames % 60 == 0) && (position < whiteKeys.length) ) {
             //only show a mwssage when we are in the bounds of the array
-            showText("Array index is: " + position, 400, 250);
-
+            
+            Key aKey = new Key(whiteKeys[position], whiteNotes[position]);
+            addObject(aKey, 54 + position * 63,140);
             //Say Hello to say everyone in the class!
             //showText("Hello " +peopleInClass[frames/60], 400, 170);
 
